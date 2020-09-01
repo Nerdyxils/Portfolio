3541668307918dda0bf3e3f61ebc6a5af7d30b29
Nerdyxils <silasabiodun25@gmail.com>
2020-09-01 19:08:09 +0100

    Added mobile icon

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const buttons = document.querySelectorAll('button');
+const mobileNav = document.getElementById('mymobilenav');
+const mobileIcon = document.getElementById('mobile_icon');
+const body = document.getElementById('nav');
 
 buttons.forEach(function(button){
     button.addEventListener('click', function(){
@@ -13,3 +16,17 @@ buttons.forEach(function(button){
         };
     })
 });
+
+
+
+body.addEventListener('click', showNav);
+function showNav() {
+    if (mobileNav.style.display === 'none') {
+
+        mobileNav.style.display = 'block';
+        mobileIcon.src = mobileIcon.src.replace('images/icon-hamburger.svg', 'images/icon-close.svg');
+    } else {
+        mobileNav.style.display = 'none';
+        mobileIcon.src = mobileIcon.src.replace('images/icon-close.svg', 'images/icon-hamburger.svg');
+    }
+}
