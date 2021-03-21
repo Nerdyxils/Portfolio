fd57a6a9ec2100bdc54aecde3943e8b964aeaa81
Nerdyxils <silasabiodun25@gmail.com>
2021-03-21 20:37:19 +0100

    mobile view fix

--- js/script.js
+++ js/script.js
@@ -127,18 +127,4 @@ const AnimateOnScroll = function ({ offset } = { offset: 10 }) {
   
   const animation = new AnimateOnScroll(options);
 
-
-  // ADD CLASS ACTIVE TO NAV MENU ON SCROLL
-$(window).scroll(function(){
-  var scrollTop = $(document).scrollTop();
-  var anchors = $('body').find('section');
-  
-  for (var i = 0; i < anchors.length; i++){
-      if (scrollTop > $(anchors[i]).offset().top - 50 && scrollTop < $(anchors[i]).offset().top + $(anchors[i]).height() - 70) {
-          $('nav ul li a[href="#' + $(anchors[i]).attr('id') + '"]').addClass('active');
-      } else {
-          $('nav ul li a[href="#' + $(anchors[i]).attr('id') + '"]').removeClass('active');
-      }
-  }
-});
   
